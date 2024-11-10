b5b0256cf47cb63957f28bb171fd293ed77b3c45
Dave Chedjoun <davechedjoun@gmail.com>
2024-11-10 06:38:39 +0100

    adding project

---
+++ src/main/java/com/devops/test/sample_project/controllers/Test.java
@@ -0,0 +1,13 @@
+package com.devops.test.sample_project.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+    @GetMapping("/hello")
+    String helloWorld() {
+        return "hello world";
+    }
+}
